680cffe570ec2189f70df41315c727ff03f84c85
Simon Meyborg <meyborg@syborgstudios.com>
2023-01-08 21:07:43 +0100

    finalize main

--- lib/ordinary_gem.rb
+++ lib/ordinary_gem.rb
@@ -1,5 +1,6 @@
 require "ordinary_gem/version"
 require "ordinary_gem/a_typical_class"
+require "ordinary_gem/another_typical_class"
 
 module OrdinaryGem
   class Error < StandardError; end
---
+++ lib/ordinary_gem/a_typical_class.rb
@@ -0,0 +1,41 @@
+module OrdinaryGem
+  class ATypicalClass
+    def a_instance_method_that_we_do_not_override
+      "This is the implementation of a simple instance method."
+    end
+
+    def a_instance_method_that_that_stays_the_same
+      "This is the implementation of a simple instance method."
+      "This method will stay the same in the next version."
+    end
+
+    def a_instance_method_that_will_change
+      "This is the implementation of a simple instance method."
+      "This method will be rewritten in the next version."
+    end
+
+    def a_instance_method_that_will_be_removed
+      "This is the implementation of a simple instance method."
+      "This method will be removed in the next version."
+    end
+    
+    def self.a_singleton_method_that_we_do_not_override
+      "This is the implementation of a simple singleton method."
+    end
+
+    def self.a_singleton_method_that_stays_the_same
+      "This is the implementation of a simple singleton method."
+      "This method will stay the same in the next version."
+    end
+
+    def self.a_singleton_method_that_will_change
+      "This is the implementation of a simple singleton method."
+      "This method will be rewritten in the next version."
+    end
+
+    def self.a_singleton_method_that_will_be_removed
+      "This is the implementation of a simple singleton method."
+      "This method will be removed in the next version."
+    end
+  end
+end
